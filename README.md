e98fd7d7187d335acfc781274505aaec2c7ea9fb
Samuel Nandi <nandisamuelsne@gmail.com>
2024-09-08 17:44:11 +0100

    Configure colors

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Kolandéla is an application for managing and planning expenses.
 
 The name Kolandéla comes from the Lingala word which means tracking.
+
 The idea came from a personal need to track my expenses. I think it is
 important to know how to evaluate in order to improve ; and to evaluate
 we need to have the necessary data.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    
-      content: ["./app/**/*.{js,ts,tsx}", "./components/**/*.{js,ts,tsx}"],
-    
-    presets: [require("nativewind/preset")],
-    theme: {
-      extend: {},
+
+  content: ["./app/**/*.{js,ts,tsx}", "./components/**/*.{js,ts,tsx}"],
+
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+    colors: {
+      'background': '#1C1C23',
+      'dark': '#000000',
+      'white': '#ffffff',
+      'foreground': '#666680',
+      'arange': '#FF7966',
+      'red': '#FF3B30',
+      'green': '#00FAD9',
+      'gray-light': '#A2A2B5',
+      'violate': '#AD7BFF',
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
