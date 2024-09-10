7a3f94ce0294563a907abcecfc54a3bd95cb5508
Samuel Nandi <nandisamuelsne@gmail.com>
2024-09-10 12:41:47 +0100

    Add neccesary package for charts

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,15 @@
 module.exports = function(api) {
   api.cache(true);
-  let plugins = [];
-  
-  
-
-  
+  let plugins = [
+    "react-native-reanimated/plugin"
+  ];
 
   return {
-    
       presets: [
         ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
         'nativewind/babel',
       ],
-    
+
     plugins,
   };
 };
