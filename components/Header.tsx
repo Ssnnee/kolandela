369c282448aa2a6d9c808b2878613f242a882e7e
Samuel Nandi <nandisamuelsne@gmail.com>
2024-09-12 21:24:21 +0100

    Add a create Button to the Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,9 +16,18 @@ export default function  Header(
         w-full bg-${bgColor ? bgColor: 'green'}
         flex-row items-center justify-between pt-14 px-6
       `}>
-      <Text className={`text-${textColor}`}></Text>
+      <View>
+        <TouchableOpacity
+          className='h-8 w-8 items-center justify-center rounded-full text-white  bg-transparent border-2 border-foreground '
+          onPress={onPress}
+        >
+          <Text className='text-foreground font-bold'>+</Text>
+        </TouchableOpacity>
+      </View>
       <Text className={`text-${textColor} text-xl font-bold`}>{title && title}</Text>
-      <Image source={require('~/assets/icons/settings.png')} className='h-8 w-8' />
+        <TouchableOpacity className=''>
+            <Image source={require('~/assets/icons/settings.png')} className='h-8 w-8' />
+        </TouchableOpacity>
     </View>
   );
 }
