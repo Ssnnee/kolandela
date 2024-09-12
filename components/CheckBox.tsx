1b4e010bb555d81c4e405abd0f02f91a98b306e6
Samuel Nandi <nandisamuelsne@gmail.com>
2024-09-12 15:18:44 +0100

    Make the CheckBox usable

--- components/CheckBox.tsx
+++ components/CheckBox.tsx
@@ -1,16 +1,21 @@
-import { useState } from 'react';
 import { Pressable } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-export default function  Checkbox() {
-  const [checked, setChecked] = useState(false);
+type CheckBoxProps = {
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+};
+
+export default function  Checkbox(
+  { checked, onChange }: CheckBoxProps
+) {
 
   return (
     <Pressable
       className={
         `h-6 w-6 rounded-full bg-transparent border-2 border-foreground ${checked && 'border-green'}`
       }
-      onPress={() => setChecked(!checked)}>
+      onPress={() => onChange(!checked)}>
       {checked && <Ionicons name='checkmark' size={15} color='#00FAD9' className='left-1'/>}
     </Pressable>
   );
