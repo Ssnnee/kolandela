6a268bbb5e3eb3ffdd3d68c3e2d18d075fe8ccf4
Samuel Nandi <nandisamuelsne@gmail.com>
2024-09-14 14:41:30 +0100

    Used hexcolor instead

--- app/(tabs)/budget/index.tsx
+++ app/(tabs)/budget/index.tsx
@@ -5,59 +5,11 @@ import BottomSheet from '~/components/BottomSheet';
 import { ButtonCard } from '~/components/ButtonCard';
 import Header from '~/components/Header';
 import {
-  LineChart,
-  BarChart,
-  PieChart,
   ProgressChart,
-  ContributionGraph,
-  StackedBarChart
 } from "react-native-chart-kit";
-import { useTheme } from '@react-navigation/native';
 import CategoryCard from '~/components/CategoryCard';
 
-const screenWidth = Dimensions.get("window").width;
-const screenHeight = Dimensions.get("window").height;
-
-// const { colors } = useTheme();
-//   const data = {
-//     labels: [
-//       "Web servers",
-//       "Operating systems",
-//       "Programming languages",
-//       "JavaScript",
-//       "Web Frameworks",
-//     ], // optional
-//     data: [0.2, 0.6, 0.8, 0.1, 0.25],
-//     colors: [
-//       "rgba(255, 0, 0,0.5)",
-//       "rgba(238, 130, 238,0.6)",
-//       "rgba(106, 90, 205,0.5)",
-//       "rgba(60, 179, 113,0.2)",
-//       "rgba(255, 172, 71 , 0.3)",
-//     ],
-//   };
-
-const chartConfig = {
-  backgroundGradientFrom: "#0E0E12",
-  backgroundGradientFromOpacity: 0,
-  backgroundGradientTo: "#08130D",
-  backgroundGradientToOpacity: 0.5,
-  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-  strokeWidth: 2, // optional, default 3
-  barPercentage: 0.5,
-  useShadowColorFromDataset: false // optional
-};
-
-
-// const chartConfig = {
-//   useShadowColorFromDataset: false,
-//   backgroundGradientFrom: "#0E0E12",
-//   backgroundGradientTo: "#0E0E11",
-//   color: (opacity = 1) => `rgba(255, 121, 102, ${opacity})`,
-// };
-
 export default function CategoryPage() {
-  const screenWidth = Dimensions.get("window").width;
   const income = 100000;
   const expenses = 100;
   const percentage = (expenses / income) * 100;
@@ -119,37 +71,19 @@ export default function CategoryPage() {
             title='Transport'
             total={10088}
             percentage={40}
-            color='orange'
+            color='#FF7966'
           />
           <CategoryCard
             title='Divertissement'
             total={79898}
             percentage={80}
-            color='green'
+            color='#00FAD9'
           />
           <CategoryCard
             title='Securité'
             total={79898}
             percentage={60}
-            color='violate'
-          />
-          <CategoryCard
-            title='Transport'
-            total={10088}
-            percentage={40}
-            color='orange'
-          />
-          <CategoryCard
-            title='Divertissement'
-            total={79898}
-            percentage={80}
-            color='green'
-          />
-          <CategoryCard
-            title='Securité'
-            total={79898}
-            percentage={60}
-            color='violate'
+            color='#AD7BFF'
           />
           </ScrollView>
         <BottomSheet bottomSheetRef={bottomSheetRef}>
