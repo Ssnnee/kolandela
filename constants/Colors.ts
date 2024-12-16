e3ce839d08009a9bc30a9136a3cc5814edd94231
Samuel Nandi <nandisamuelsne@gmail.com>
2024-12-16 23:02:06 +0100

    Change the colors scheme

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,35 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = '#FF7966';
+const tintColorDark = '#00FAD9';
 
 export const Colors = {
   light: {
     text: '#11181C',
-    background: '#fff',
+    background: '#FFFFFF',
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: '#666680',
+    tabIconDefault: '#83839C',
     tabIconSelected: tintColorLight,
+    backgroundVariant: '#F0F0F5',
+    primary: '#AD7BFF',
+    error: '#FF3B30',
+    success: '#00C4A7',
+    grayLight: '#A2A2B5',
   },
   dark: {
     text: '#ECEDEE',
-    background: '#151718',
+    background: '#0E0E12',
     tint: tintColorDark,
     icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    tabIconDefault: '#666680',
     tabIconSelected: tintColorDark,
+    backgroundVariant: '#26262F',
+    primary: '#AD7BFF',
+    error: '#FF3B30',
+    success: '#00FAD9',
+    grayLight: '#A2A2B5',
   },
 };
+
