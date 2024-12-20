69b39db93019d87d82cc277e3d3d19060334025b
Samuel Nandi <nandisamuelsne@gmail.com>
2024-12-20 18:04:48 +0100

    test connexion and displaying data

--- db/initialize-drizzle.ts
+++ db/initialize-drizzle.ts
@@ -1,7 +1,7 @@
 import * as SQLite from 'expo-sqlite';
 import { drizzle } from 'drizzle-orm/expo-sqlite';
 
-export const sqliteDB = SQLite.openDatabaseSync('kolandela');
+export const sqliteDB = SQLite.openDatabaseSync('kolandela', { enableChangeListener: true });
 
 export const db = drizzle(sqliteDB);
 
