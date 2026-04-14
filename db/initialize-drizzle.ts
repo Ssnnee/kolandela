1b270de2e367a34b5b29d557bf226c4ac3cd5eec
Samuel Nandi <nandisamuelsne@gmail.com>
2026-04-14 10:01:53 +0100

    Initial commit after restarting from scratch

--- db/initialize-drizzle.ts
+++
@@ -1,7 +0,0 @@
-import * as SQLite from 'expo-sqlite';
-import { drizzle } from 'drizzle-orm/expo-sqlite';
-
-export const sqliteDB = SQLite.openDatabaseSync('kolandela', { enableChangeListener: true });
-
-export const db = drizzle(sqliteDB);
-
